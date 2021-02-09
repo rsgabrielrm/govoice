eb22079467db0c436b9edc976dd445bcd180aae9
Gabriel <gabrielkcm@hotmail.com>
2021-02-09 01:58:38 -0300

    :seedling: Created number factory

--- app/Models/Number.php
+++ app/Models/Number.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Number extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     const NUMBER_STATUS_OPTIONS = ['active', 'suspended', 'cancelled'];
 
