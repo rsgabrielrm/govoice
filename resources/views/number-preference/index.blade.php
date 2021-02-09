418ae9faf2b9383befae14ad218232a0d82f29fe
Gabriel <gabrielkcm@hotmail.com>
2021-02-08 22:59:13 -0300

    :sparkles: Created number preference crud

--- resources/views/number-preference/index.blade.php
+++ resources/views/number-preference/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Numbers') }}
+            {{ __('Number - preferences') }}
         </h2>
     </x-slot>
 
@@ -11,30 +11,48 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="h-screen">
 
+                        <div class="text-left">
+                            <div class="w-1/2 xl:w-2/4 px-3">
+                                <div class="w-full bg-white border text-blue-700 rounded-lg flex items-center p-6 mb-6 xl:mb-0">
+                                    <svg class="w-16 h-16 fill-current mr-4 lg:block" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="address-book" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path fill="currentColor" d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z"></path>
+                                    </svg>
+
+                                    <div class="text-gray-700">
+                                        <p class="font-semibold text-3xl">{{ $customer->name }}</p>
+                                        <p>{{ $customer->document }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="text-right">
                             <a
                                 class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-green-500 rounded-full shadow ripple hover:shadow-lg hover:bg-green-600 focus:outline-none"
-                                href="{{ route('numbers.create') }}"
+                                href="{{ route('number.preferences.create', $number->id) }}"
                             >
-                                    New number
+                                    New preference number
                             </a>
                         </div>
 
                         <div class="pt-6 text-center">
+
                             <table class="min-w-full divide-y divide-gray-200">
                                 <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Id
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Customer
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Number
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Status
+                                        Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Value
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Auto attendant
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Voicemail enabled
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Actions
@@ -42,43 +60,39 @@
                                 </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200 ">
-                                @forelse($numbers as $number)
+                                @forelse($numberPreferences as $numberPreference)
                                     <tr>
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class=" items-center">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ $number->id }}
-                                                </div>
+                                            <div class="text-sm text-gray-900">
+                                                {{ $numberPreference->number->number }}
                                             </div>
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="text-sm text-gray-900">
-                                                {{ $number->customer->name }}
+                                                {{ $numberPreference->name }}
                                             </div>
                                         </td>
-
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="text-sm text-gray-900">
-                                                {{ $number->number }}
+                                                {{ $numberPreference->value }}
                                             </div>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                              {{ ucfirst($number->status) }}
+                                              {{ $numberPreference->auto_attendant ? 'Enabled' : 'Disabled' }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ $numberPreference->voicemail_enabled ? 'Enabled' : 'Disabled' }}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                            <a href="{{ route('number.preferences.index', $number->id) }}"
-                                                class="inline-block p-3 text-center text-white transition bg-blue-700 rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
-                                            >
-                                                <svg class="w-5 h-5 text-white" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="address-book" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z"></path></svg>
-                                            </a>
-                                            <a href="{{ route('numbers.edit', $number->id) }}"
+
+                                            <a href="{{ route('number.preferences.edit', [$numberPreference->number_id, $numberPreference->id]) }}"
                                                 class="inline-block p-3 text-center text-white transition bg-green-500 rounded-full ripple hover:bg-green-600 focus:outline-none"
                                             >
                                                 <svg class="w-5 h-5 text-white" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="edit" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path></svg>
                                             </a>
-                                            <a onclick="openModal('deleteConfirme', '{{ $number->number }}', '{{ route('numbers.destroy', $number->id) }}')"
+                                            <a onclick="openModal('deleteConfirme', '{{ $numberPreference->name }}', '{{ route('number.preferences.destroy',[$numberPreference->number_id, $numberPreference->id]) }}')"
                                                 class="inline-block p-3 text-center text-white transition bg-red-500 rounded-full shadow ripple hover:shadow-lg hover:bg-red-600 focus:outline-none"
                                             >
                                                 <svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -94,7 +108,7 @@
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td colspan="5" class="px-6 py-4 text-center whitespace-nowrap">
+                                        <td colspan="7" class="px-6 py-4 text-center whitespace-nowrap">
                                             <h3>There are no registered numbers</h3>
                                         </td>
                                     </tr>
@@ -102,7 +116,7 @@
 
                                 </tbody>
                             </table>
-                            {{ $numbers->links() }}
+                            {{ $numberPreferences->links() }}
                         </div>
                     </div>
                 </div>
